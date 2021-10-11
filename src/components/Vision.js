76c47c59d3f14762a6f9bee226a6ba78bd7cc951
Anjana <anjanakta@gmail.com>
2021-10-11 11:31:44 +0530

    update name

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -7,7 +7,7 @@ function Vision() {
                 <h1 >Vision</h1>
                 <p class="vision-content">The Innovation and Entrepreneurship Development Cell aims to help students develop a spirit of innovation and entrepreneurship and to undertake major initiatives to assist the promotion of the same in the region by providing them the facilities to create the products and the services that will take us together towards a brighter future.</p>
                 <h1 class="vision-heading">About Us</h1>
-                <p class="vision-content">The Innovation and Entrepreneurship Development Cell of TKMCE is an organisation that aims to promote the institutional vision of transforming youngsters into technological entrepreneurs and innovative leaders. The initiative is to address the rising trend of corporate job culture and establish a platform to pursue ideas and businesses at an early stage. We at IEDC TKMCE host various workshops, speaker sessions, competitions to develop skill sets and provide essential resources to take up entrepreneurship as a career.</p>
+                <p class="vision-content">The Innovation and Entrepreneurship Development Cell of xxxx is an organisation that aims to promote the institutional vision of transforming youngsters into technological entrepreneurs and innovative leaders. The initiative is to address the rising trend of corporate job culture and establish a platform to pursue ideas and businesses at an early stage. We at IEDC xxxx host various workshops, speaker sessions, competitions to develop skill sets and provide essential resources to take up entrepreneurship as a career.</p>
             </div>    
         </div>    
     )
